cc4a5c9047e2ad491f76206765cbf49177e81fff
Anna M <83anei@gmail.com>
2021-10-05 18:45:28 +0100

    Add new option 'Add'.

--- bookstore.py
+++ bookstore.py
@@ -3,6 +3,11 @@ def exiting():
     exit()
 
 
+def add_item(name):
+    items.append(name)
+    print(items)
+
+
 def get_items():
     print(f"{'Name': <31} {'Quantity': <10} {'Unit': <8} {'Unit price (PLN)': <8}")
     for item in items:
@@ -13,17 +18,26 @@ if __name__ == "__main__":
     print("Bookstore Management System")
     print("Exit - exit the program.")
     print("Show - display stock.")
+    print("Add - add an item.")
 
-    choice = input("Choose one from the options: ")
-    choice = choice.lower()
+    while True:
+        choice = input("Choose one from the options: ")
+        choice = choice.lower()
 
-    items = [["The tower", 50, "pcs", 44.99],
-             ["Ania z zielonego wzgorza", 20, "pcs", 32.99],
-             ["Animal Farm", 10, "pcs", 15.99]]
+        items = [["The Tower", 50, "pcs", 44.99],
+                 ["The Witcher", 20, "pcs", 32.99],
+                 ["Animal Farm", 10, "pcs", 15.99]]
 
-    if choice == "exit":
-        print("Exiting... bye!")
-    elif choice == "show":
-        print(get_items())
-    else:
-        print("Wrong option, exiting... bye!")
+        if choice == "exit":
+            print("Exiting... bye!")
+            break
+        elif choice == "show":
+            print(get_items())
+            continue
+        elif choice == "add":
+            new_item = [input("To add an item please provide name, quantity, unit and unit price separated with comma: ")]
+            add_item(new_item)
+            continue
+        else:
+            print("Wrong option, exiting... bye!")
+            break
